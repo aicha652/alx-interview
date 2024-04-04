2bc5246d56d264d5a032c56440241e19f8a8ed05
aicha652 <aichaazlf652@gmail.com>
2024-04-04 15:18:26 +0200

    fix

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+""" method that determines if a given data
+set represents a valid UTF-8 encoding"""
+
+
 def validUTF8(data):
-    try:
-        data_decode = data.decode('utf-8')
-        return True
-    except UnicodeDecodeError:
-        return False
+    """define a function"""
+    for item in data:
+        try:
+            data_decode = data.decode('utf-8')
+        except UnicodeDecodeError:
+            return False
+    return True
