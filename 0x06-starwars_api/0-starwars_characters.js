27d82daca5b4847f1d3a4d87c1e8f51c124b2d21
aicha652 <aichaazlf652@gmail.com>
2024-04-18 12:38:13 +0200

    fix order

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -13,7 +13,7 @@ request.get(url, async (error, response, body) => {
     await new Promise((resolve) => {
       request.get(character, (error, response, body) => {
         if (error) {
-          reject(error);
+          resolve();
         }
         const characterData = JSON.parse(body);
         console.log(characterData.name);
