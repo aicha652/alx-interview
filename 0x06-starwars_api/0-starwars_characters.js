904bafdc0b87c32b2c86da2e0200834a3f4c3b82
aicha652 <aichaazlf652@gmail.com>
2024-04-18 11:23:05 +0200

    fix outputs

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,13 +6,13 @@ const request = require('request');
 
 const url = 'https://swapi-api.alx-tools.com/api/films/' + arg + '/';
 
-request.get(url, (error, response, body) => {
+request.get(url, async (error, response, body) => {
   if (error) console.log(error);
-  const data = JSON.parse(body);
+  const data = await JSON.parse(body);
   for (const character of data.characters) {
-    request.get(character, (error, response, body) => {
+    request.get(character, async (error, response, body) => {
       if (error) console.log(error);
-      const characterData = JSON.parse(body);
+      const characterData = await JSON.parse(body);
       console.log(characterData.name);
     });
   }
